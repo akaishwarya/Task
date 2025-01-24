04ff43e98b06f6ae869969db7ae4837438df437e
SanthoshKumarS29 <santhoshvk090@gmail.com>
2025-01-24 21:32:59 +0530

    Fixing the error

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ function App() {
 
   return (
     <div> 
-      <div className='flex '>
+      {/* <div className='flex '>
             <Sidebar sidebarToggle={sidebarToggle}/>
             <h1>Name</h1>
-      </div>
+      </div> */}
       <Routes>
         {/* Route for the Dashboard page  */}
         <Route path="/Task/" element={<Dashboard sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle} />} />
